f66772f215c6a6aec0c17349610d93ae2f56e625
Adam <ahanson845@gmail.com>
2018-09-02 08:57:53 -0500

    updated contacts app

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
   namespace :api do
-    get "/one_contact_url" => 'contacts#one_contact_action'
-    get "/many_contacts_url" => 'contacts#many_contacts_action'
+    get "/contacts" => 'contacts#index'
+    get "/contacts/:id" => 'contacts#show'
+    post "/contacts" => 'contacts#create'
+    patch "/contacts/:id" => 'contacts#update'
+    delete "/contacts/:id" => 'contacts#destroy'
   end
 end
